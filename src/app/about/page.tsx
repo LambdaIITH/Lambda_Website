e6ad79d0eb5fb2c475cb6b0c9571f3f3de583ed6
Rajat <rajathegde2006@gmail.com>
2026-01-13 20:53:46 +0530

    fixed issue in about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -84,21 +84,21 @@ export default function AboutPage() {
           <p className="text-5xl text-[#D2A8FF] font-bold">Upcoming Events</p>
         </div>
         <div className="mt-20 flex justify-center">
-          <div className="w-[900px] flex flex-col">
+          <div className="w-full max-w-[900px] flex flex-col">
 
             {events.map((event, i) => (
-              <div key={i} className="relative flex items-start gap-10">
+              <div key={i} className="relative flex flex-col md:flex-row items-center md:items-start gap-6 md:gap-10">
 
                 {/* Date */}
-                <p className="w-[90px] text-right pr-4 text-white/70 text-sm mt-6">
+                <p className="hidden md:block w-[90px] text-right pr-4 text-white/70 text-sm mt-6">
                   {event.date}
                 </p>
 
                 {/* Card wrapper (this owns the timeline) */}
-                <div className="relative pl-12 border-l pb-10 border-purple-500/60">
+                <div className="relative flex w-3/4 flex-wrap md:w-full md:pl-12 md:border-l pb-10 border-purple-500/60">
 
                   {/* Dot */}
-                  <div className="absolute -left-[9px] top-6 flex items-center justify-center">
+                  <div className="hidden md:flex absolute -left-[9px] top-6 items-center justify-center">
                     <div className="w-4 h-4 rounded-full bg-fuchsia-500 shadow-[0_0_12px_#d946ef]" />
                     <div className="absolute w-9 h-9 rounded-full border border-fuchsia-500/40" />
                   </div>
